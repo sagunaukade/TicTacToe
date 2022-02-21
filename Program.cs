eee597339b64d0ea7a2a0d8fbceedbeb81cc145f
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-21 09:56:57 +0530

    First Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Initializing()
+        {
+            char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            Console.WriteLine("{0} | {1} | {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("{0} | {1} | {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("{0} | {1} | {2}", arr[7], arr[8], arr[9]);
+
+
+        }
+        public static void Main(string[] args)
+        {
+            Program.Initializing();
+            Console.ReadLine();
+        }
+        
+    }
+}
